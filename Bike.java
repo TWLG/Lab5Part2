0efdabee3314572ab77e5d78e08a3cc0ff59d7d9
deezus13 <100430008+deezus13@users.noreply.github.com>
2022-03-25 10:11:19 -0400

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,5 +1,6 @@
 class Bike extends LandTransportation {
-
+    
+    // constructor
     public Bike(String type, double cost, String purchaseType, int avgSpeed, int capacity, String departureTime, String departureLocation) {
     	super();
     	this.vehicleType = "Bike";
